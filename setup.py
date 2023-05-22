4fcca3a92f8e8355ba0506a64e212792f0aa69e6
Evelyn Yang (Xinmei) <98622311+elle2222@users.noreply.github.com>
2023-05-21 22:00:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md","r") as fh:
     long_des = fh.read()
 
 setup(
-    name="homoglyphs_testpypi",
-    version="0.1.5",
-    author="eve",
-    author_email="xmyang.research@gmail.com",
-    description="A Python package for fuzzy matching Chinese(simplified and traditional), Japanese and Korean, using character similarity trained using ViT transformer",
+    name="HomoglyphsCJK",
+    version="0.0.1",
+    author="HomoglyphsCJK Team",
+    author_email="homoglyphscjk@gmail.com",
+    description="An easy Python package for fuzzy matching Chinese(simplified and traditional), Japanese and Korean, using character similarity trained from ViT transformer",
     long_description=long_des,
     long_description_content_type="text/markdown",
-    url="https://github.com/dell-research-harvard/homoglyphsFuzzyCJK.git",
+    url="https://github.com/dell-research-harvard/HomoglyphsCJK.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
