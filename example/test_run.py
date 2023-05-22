5cd232da92692946b0b312de5fbb1bc0b0f46e71
Evelyn Yang (Xinmei) <98622311+elle2222@users.noreply.github.com>
2023-05-21 22:54:11 -0400

    Update test_run.py

--- example/test_run.py
+++ example/test_run.py
@@ -6,10 +6,10 @@ df_1 = pd.read_csv('./df_1_small.csv')
 df_2 = pd.read_csv('./df_2_small.csv')
 
 ## Dataframe merge
-dataframe_merged = homoglyph_merge('ja',df_1,df_2,'ocred_text','truth_text',homo_lambda=1, insertion=1, deletion=1,parallel=True,num_workers=4)
+dataframe_merged = homoglyph_merge('ko',df_1,df_2,'ocred_text','truth_text',homo_lambda=1, insertion=1, deletion=1,parallel=True,num_workers=4)
 
 ## Non-parallel
-dataframe_merged = homoglyph_merge('ja',df_1,df_2,'ocred_text','truth_text',homo_lambda=1, insertion=1, deletion=1) 
+dataframe_merged = homoglyph_merge('ko',df_1,df_2,'ocred_text','truth_text',homo_lambda=1, insertion=1, deletion=1) 
 '''
 Parameters:
 lang
@@ -24,3 +24,5 @@ dataframe_merged.to_csv('./merged.csv')
 download_dict('zhs')
 homoglyph_distance('苏萃乡','小苏莽乡',homo_lambda=1, insertion=1, deletion=1)
 
+
+# %%
