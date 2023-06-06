1044f6139a5d0432c8422367cb57c0e16186b5f7
Evelyn Yang (Xinmei) <98622311+elle2222@users.noreply.github.com>
2023-06-06 01:54:36 -0400

    fix encoding error for windows

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os
 from setuptools import find_packages, setup
 
-with open("README.md","r") as fh:
+with open("README.md","r",encoding='utf-8') as fh:
     long_des = fh.read()
 
 setup(
     name="HomoglyphsCJK",
     packages=["HomoglyphsCJK"],
-    version="0.1.2",
+    version="0.1.4",
     author="HomoglyphsCJK Team",
     author_email="homoglyphscjk@gmail.com",
     description="An easy Python package for fuzzy matching Chinese(simplified and traditional), Japanese and Korean, using character similarity trained from ViT transformer",
