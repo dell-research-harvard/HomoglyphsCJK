ed2a26e68f27129b05e4ef1123126b50834968b6
Evelyn Yang (Xinmei) <98622311+elle2222@users.noreply.github.com>
2023-10-12 20:22:29 -0400

    Update __init__.py

--- HomoglyphsCJK/__init__.py
+++ HomoglyphsCJK/__init__.py
@@ -1,3 +1,3 @@
 
-from HomoglyphsCJK.hg_functions import download_dict,homoglyph_distance,homoglyph_merge, homoglyph_pairwise_distance
+from HomoglyphsCJK.hg_functions import download_dict,homoglyph_distance,hg_merge, hg_distance
 
